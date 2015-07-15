181c59d555d80b917b51861ec034aba2fb306207
Ho Minh Hiep <hiephm@smartosc.com>
2015-07-15 11:33:38 +0700

    update alias and path

---
+++ custom/hiephm.aliases.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+### Basic
+alias ll="ls -alh --color=auto"
+
+### Git
+alias g="git"
+alias gs="git status"
+alias gm="git merge"
+alias ga="git add && git status"
+alias gaa="git add -A && git status"
+alias gfa="git fetch --all"
+
+alias gk="gitk &"
+alias gkall="gitk --all &"
+
+### Tool
+alias edit="notepad++.exe"
+alias composer="composer.phar"
+
+### Remote
+alias dev="ssh hiephm@dev"
+
